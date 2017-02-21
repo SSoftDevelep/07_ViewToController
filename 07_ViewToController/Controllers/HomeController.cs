8ecb0dc099d9b8ef6ec2e70013468040e1c91994
SsoftDevelep <saitemresolmaz@gmail.com>
2017-02-21 16:22:47 +0300

    View To Controller

---
+++ 07_ViewToController/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _07_ViewToController.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [HttpGet]
+        public ActionResult HomePage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult HomePage(string text1,bool check1,string list1)
+        {
+            var t1 = Request.Form["text1"];
+            var l1 = Request.Form["list1"];
+            var c1=  Request.Form.GetValues("check1")[0];  // isaretliyse true,false degilse false,true alir. 0. item ilk secilen oluyor.
+
+            return View();
+        }
+    }
+}
